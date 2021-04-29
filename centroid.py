537d015a5de03bce8fcf23b8090e56632776cb29
OmarMo7 <omar7.first@gmail.com>
2021-04-29 08:04:52 +0200

    Deleting unnecessaries & changing names

--- centroid.py
+++ centroid.py
@@ -4,8 +4,8 @@ from mlxtend.data import loadlocal_mnist
 from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
 
-
-def readAndReshapeData(imagesPath, labelsPath):
+# Extracting & reshaping image to 28 * 28 pixels aspect
+def ExtractAndReshape(imagesPath, labelsPath):
     images, images_labels = loadlocal_mnist(
         images_path=imagesPath,
         labels_path=labelsPath)
@@ -14,45 +14,21 @@ def readAndReshapeData(imagesPath, labelsPath):
     images = np.array(images)
     return images, images_labels
 
-
-def preprocessingImage(input_images):
-    input_images = input_images.reshape(len(input_images), 784)
-    outputImages_list = []
-
-    for image in input_images:
-        # change image to BGR
-        bgrImage = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        # change image gray scale
-        grayImage = cv2.cvtColor(bgrImage, cv2.COLOR_BGR2GRAY)
-        ret, binaryImage = cv2.threshold(
-            grayImage, 127, 255, 0)  # change image to binaryImage
-        moment = cv2.moments(binaryImage)  # calculate moment to get centroid
-        cX = int(moment["m10"] / moment["m00"])  # calculate x
-        cY = int(moment["m01"] / moment["m00"])  # calculate y
-        # add circle at centroid point
-        output_Image = cv2.circle(image, (cX, cY), 5, (255, 255, 255), -1)
-        # add centroid to feature vector
-        outputImages_list.append(output_Image)
-
-    outputImages_list = np.array(outputImages_list)
-    return outputImages_list
-
-
+# Split a matrix into sub-matrices.
 def split(array, nrows, ncols):
-    #Split a matrix into sub-matrices.
     r, h = array.shape
     return (array.reshape(h // nrows, nrows, -1, ncols)
             .swapaxes(1, 2)
             .reshape(-1, nrows, ncols))
 
-
-def centerOfMass(image):
+# The main function to get the 
+def centerOfMass(matrix):
     Sumx, Sumy, num = 0, 0, 0
     cx, cy = 0, 0
 
-    for i in range(0, len(image)):
-        for j in range(0, len(image)):
-            if image[i][j] != 0:  # if pixel has a value
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix)):
+            if matrix[i][j] != 0:  # if pixel has a value
                 Sumx += i  # add index of x-axis
                 Sumy += j  # add index of y-axis
                 num += 1  # get their number
@@ -60,6 +36,7 @@ def centerOfMass(image):
     if num != 0:
         cx = Sumx / num  # get point of centroid on x-axis
         cy = Sumy / num  # get point of centroid on y-axis
+
     return cx, cy
 
 
@@ -70,9 +47,9 @@ def extractFeatures(imagesList):
         featureVector = []
         # split matrix into 16 sub matrices
         for matrix in split(image, 7, 7):
-            cx1, cy1 = centerOfMass(matrix)
+            cx, cy = centerOfMass(matrix)
             # collect centroid of sub matrices into one list
-            featureVector.append((cx1, cy1))
+            featureVector.append((cx, cy))
         inputFeatures.append(featureVector)  # get feature vector of all images
 
     print(inputFeatures)
@@ -88,14 +65,6 @@ def applyKNN(trainFeatures, trainLabels, testFeatures):
     return prediction
 
 
-def openCv_model(trainFeatures, testFeatures, trainLabels, testLabels):
-    train_inputs_openCV = preprocessingImage(trainFeatures)
-    test_inputs_openCV = preprocessingImage(testFeatures)
-
-    prediction = applyKNN(train_inputs_openCV, trainLabels, test_inputs_openCV)
-    return accuracy_score(testLabels, prediction)
-
-
 def second_model(trainFeatures, testFeatures, trainLabels, testLabels):
     train_inputs = extractFeatures(trainFeatures)
     test_inputs = extractFeatures(testFeatures)
@@ -108,15 +77,15 @@ def second_model(trainFeatures, testFeatures, trainLabels, testLabels):
 
 
 def main():
-    train_features, train_labels = readAndReshapeData(
+    train_features, train_labels = ExtractAndReshape(
         "train-images.idx3-ubyte", "train-labels.idx1-ubyte")
-    test_features, test_labels = readAndReshapeData(
+    test_features, test_labels = ExtractAndReshape(
         "t10k-images.idx3-ubyte", "t10k-labels.idx1-ubyte")
 
-    accuracy_score_for_openCv = openCv_model(
-        train_features, test_features, train_labels, test_labels)
     accuracyScore = second_model(
         train_features, test_features, train_labels, test_labels)
 
-    print("Accuracy Score for openCv =", accuracy_score_for_openCv * 100, "%")
     print("Accuracy Score =", accuracyScore * 100, "%")
+
+
+main()
